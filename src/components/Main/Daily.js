1107c36a26cac0fc001dd994f51965d675f6f4c4
nichenqin1001 <nichenqin@hotmail.com>
2017-06-10 11:22:24 +0800

    use babel-polyfill with webpack

--- src/components/Main/Daily.js
+++ src/components/Main/Daily.js
@@ -4,7 +4,12 @@ import DailyWeather from './DailyWeather';
 const Daily = ({ daily, convertTempType, onSelectDailyWeather }) => {
   return (
     <div className="daily clearfix">
-      {daily.data.map(dailyWeather => <DailyWeather key={dailyWeather.temperatureMaxTime} onSelectDailyWeather={onSelectDailyWeather} convertTempType={convertTempType} dailyWeather={dailyWeather} />)}
+      {daily.data.map(dailyWeather =>
+        <DailyWeather
+          key={dailyWeather.temperatureMaxTime}
+          onSelectDailyWeather={onSelectDailyWeather}
+          convertTempType={convertTempType}
+          dailyWeather={dailyWeather} />)}
     </div>
   );
 };
